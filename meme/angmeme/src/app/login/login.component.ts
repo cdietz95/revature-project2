db61c0819a112c7b5a450280c436534089844a7d
JONBONIN\Jonathan <jon.e.bonin@gmail.com>
2018-11-10 17:19:52 -0500

    finished wiring of login to JDBC

--- meme/angmeme/src/app/login/login.component.ts
+++ meme/angmeme/src/app/login/login.component.ts
@@ -25,4 +25,67 @@ export class LoginComponent implements OnInit {
     // document.getElementById('haveLogin').checked = false
   }
 
+  onLogin(event){
+    event.preventDefault();
+    fetch('http://localhost:8080/user-api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Authorization': 'Bearer ' + localStorage.getItem('profile')
+
+      },
+      body: JSON.stringify({
+        username: (<HTMLInputElement>document.getElementById('uname1')).value,
+        password: (<HTMLInputElement>document.getElementById('pwd')).value
+      })
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        if (res.status == 401 || res.status == 400) {
+          throw{};
+        }
+      }
+    }).then(token => {
+      localStorage.setItem('profile',token.idToken);
+      localStorage.setItem('username', token.userName);
+      localStorage.setItem('userId', token.userId);
+      location.href = "http://localhost:4200";
+    }).catch(error => {
+    });
+  }
+
+  onCreate(event){
+    event.preventDefault();
+    fetch('http://localhost:8080/user-api/user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Authorization': 'Bearer ' + localStorage.getItem('profile')
+
+      },
+      body: JSON.stringify({
+        username: (<HTMLInputElement>document.getElementById('uName')).value,
+        password: (<HTMLInputElement>document.getElementById('password')).value,
+        email: (<HTMLInputElement>document.getElementById('email1')).value,
+        firstName: (<HTMLInputElement>document.getElementById('fName')).value,
+        lastName: (<HTMLInputElement>document.getElementById('lName')).value,
+      })
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        if (res.status == 401 || res.status == 400) {
+          throw{};
+        }
+      }
+    }).then(token => {
+      localStorage.setItem('profile',token.idToken);
+      localStorage.setItem('username', token.userName);
+      localStorage.setItem('userId', token.userId);
+      location.href = "http://localhost:4200";
+    }).catch(error => {
+    });
+  }
+
 }
--- meme/src/main/java/com/ex/meme/web/UserController.java
+++ meme/src/main/java/com/ex/meme/web/UserController.java
@@ -1,5 +1,4 @@
 package com.ex.meme.web;
-
 import com.ex.meme.entities.Comment;
 import com.ex.meme.entities.Post;
 import com.ex.meme.entities.TokenDTO;
@@ -9,15 +8,12 @@ import com.ex.meme.services.PostService;
 import com.ex.meme.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 @RestController
 @RequestMapping("/user-api")
 public class UserController
@@ -25,26 +21,20 @@ public class UserController
     PostService postService;
     UserService userService;
     CommentService commentService;
-
     @Autowired
     public void setCommentService(CommentService commentService) { this.commentService = commentService;}
-
     @Autowired
     public void setPostService(PostService postService) {
         this.postService = postService;
     }
-
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
     }
-
     @PostMapping(value = "/login", consumes="application/json")
     //@GetMapping(value = "/comment", consumes = "application/json")
     public void addPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
         User e = new ObjectMapper().readValue(req.getInputStream(), User.class);
-
         if(e.getUsername() == null || e.getPassword() == null)
         {
             resp.setStatus(400);
@@ -54,7 +44,6 @@ public class UserController
             //e = service.getOne(e.getEmail(), e.getHash());
             e = userService.getUserByEmailAndPassword(e.getUsername(), e.getPassword());
         }
-
         if(e != null)
         {
             try
@@ -71,22 +60,19 @@ public class UserController
                 //logger.error(ex.getMessage());
                 return;
             }
-
         }else
         {
             resp.setStatus(401);
             return;
         }
     }
-
-    @PostMapping(value = "/create", consumes="application/json")
+    //    @CrossOrigin(origins = "http://localhost:4200")
     //@GetMapping(value = "/comment", consumes = "application/json")
-    public void createUser(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-        User u = new ObjectMapper().readValue(req.getInputStream(), User.class);
-
+    @CrossOrigin(origins = "*")
+    @PostMapping(value = "/user")
+    public ResponseEntity createUser(@RequestBody User u, HttpServletResponse resp) {
         userService.addUser(u);
-
-
+        resp.setHeader("Location", "http://localhost:8080/user" + u.getId());
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
